55614223286546b153ed4a9532620c71303b907f
Nikita Stroganov <findoss@yandex.ru>
2019-07-23 16:06:38 +0300

    add git cmd deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,18 @@
-echo 'START DEPLOY'
+echo 'START DEPLOY';
+
+git checkout gh-pages
+
+cp public/CNAME CNAME
+cp public/index.html index.html
+cp public/README.md README.md
+cp public/CNAME CNAME
+
+git add -A .
+
+git commit -m 'update maps'
+
+git push origin gh-pages
+
+git checkout master
+
+echo 'START DEPLOY';
