a08ab6050d33d190bc105e96bb83d6ef8018edbb
Nikita Stroganov <findoss@yandex.ru>
2019-07-23 16:09:09 +0300

    test deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,18 @@
 echo 'START DEPLOY';
 
-git checkout gh-pages
+# git checkout gh-pages
 
-cp public/CNAME CNAME
-cp public/index.html index.html
-cp public/README.md README.md
-cp public/CNAME CNAME
+# cp public/CNAME CNAME
+# cp public/index.html index.html
+# cp public/README.md README.md
+# cp public/CNAME CNAME
 
-git add -A .
+# git add -A .
 
-git commit -m 'update maps'
+# git commit -m 'update maps'
 
-git push origin gh-pages
+# git push origin gh-pages
 
-git checkout master
+# git checkout master
 
 echo 'START DEPLOY';
