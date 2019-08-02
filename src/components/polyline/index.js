1c4edc37f10724623ed082477cdd692f2ba31086
Nikita Stroganov <findoss@yandex.ru>
2019-08-02 19:59:22 +0300

    add render polyline

---
+++ src/components/polyline/index.js
@@ -0,0 +1,16 @@
+import L from 'leaflet/dist/leaflet';
+
+export function Polyline(doc, id) {
+  console.log('rendr line');
+
+  console.log(doc);
+
+  L.polyline(doc.geometry.coordinates, {
+    color: 'lime',
+    opacity: 0.3,
+    weight: 10,
+    className: `polyline-base polyline-${doc.properties.title}`
+  })
+    .on('click', () => console.log('Polyline id = ', id))
+    .addTo(this.groups[doc.properties.title]);
+}
