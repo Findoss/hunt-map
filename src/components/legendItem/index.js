8ec73ce6ebb26c15aa09bbff32d0892bc0a94f67
Nikita Stroganov <findoss@yandex.ru>
2019-08-02 20:00:35 +0300

    update legend component

--- src/components/legendItem/index.js
+++ src/components/legendItem/index.js
@@ -8,6 +8,9 @@ export function legendItem(type) {
     case 'new-object':
       title = 'New Markers';
       break;
+    case 'zone':
+      title = 'Compounds zone';
+      break;
   }
 
   return `<div class="legend-item marker-${type}"></div><span>${title.replace('-', ' ')}</span>`;
