6af3099c3fe8f82e4b27e498a24f3e6affa08ef0
Nikita Stroganov <findoss@yandex.ru>
2019-08-09 22:41:49 +0300

    add modal component

---
+++ src/components/modal/index.js
@@ -0,0 +1,23 @@
+import './index.css';
+import template from './index.html';
+import { Component } from '../index';
+
+export class Modal extends Component {
+  constructor(placeholderId, props) {
+    super(placeholderId, props, template);
+    // this.addSwitchButtons(props.data);
+  }
+
+  addSwitchButtons(data) {
+    // Object.keys(data).forEach(mapId => {
+    //   let button = document.createElement('button');
+    //   button.value = mapId;
+    //   button.className = 'switch-map-button';
+    //   button.innerHTML = t(mapId);
+    //   button.addEventListener('click', e => {
+    //     this.triggerEvent('switchMap', e.target.value);
+    //   });
+    //   this.refs.container.appendChild(button);
+    // });
+  }
+}
