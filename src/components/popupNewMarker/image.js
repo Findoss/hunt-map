bde97fb0601504edc52857fbd4c795b70d688568
Nikita Stroganov <findoss@ayndex.ru>
2019-09-14 21:25:22 +0300

    fix tag upload new image

--- src/components/popupNewMarker/image.js
+++ src/components/popupNewMarker/image.js
@@ -1,6 +1,1 @@
-export default props => `
-<br />
-<a href="${props.url}" target="_blank" class="cursor-zoom" rel="noopener noreferrer">
-  <img height="100px" src="${props.url}" />
-</a>
-`;
+export default props => `<br /><img height='100px' src='${props.url}' data-modal='true'/>`;
