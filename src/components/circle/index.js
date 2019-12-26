aeee729d849bb5c9e8fa57ca933650e5c29e22e8
Nikita Stroganov <findoss@yandex.ru>
2019-12-26 14:35:35 +0300

    add component circle

---
+++ src/components/circle/index.js
@@ -0,0 +1,13 @@
+import './index.css';
+import L from 'leaflet/dist/leaflet';
+
+export function Circle(doc, radius, color) {
+  return L.circle(doc.geometry.coordinates, {
+    radius: radius,
+    color: color,
+    fillColor: color,
+    fillOpacity: 0.1,
+    dashArray: [20, 15],
+    className: 'circle'
+  });
+}
