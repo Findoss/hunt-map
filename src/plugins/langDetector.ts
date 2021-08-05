77ff3dac19bb2d6742d5e5947e227fbfd9ad7e97
Nikita Stroganov <findoss@yandex.ru>
2021-08-05 21:37:03 +0300

    up

---
+++ src/plugins/langDetector.ts
@@ -0,0 +1,23 @@
+import { getUrlData } from '../utils/URL';
+import type { DetectorOptions } from 'i18next-browser-languagedetector';
+
+export const langDetector = {
+  name: 'customDetector',
+
+  lookup(options: DetectorOptions): string | string[] | undefined {
+    console.log('customDetector');
+    // console.log(options);
+
+    const [idLang] = getUrlData().path;
+    if (idLang) {
+      return idLang;
+    }
+    return 'en';
+  },
+
+  cacheUserLanguage(lng: string, options: DetectorOptions): void {
+    // options -> are passed in options
+    // lng -> current language, will be called after init and on changeLanguage
+    // store it
+  },
+};
