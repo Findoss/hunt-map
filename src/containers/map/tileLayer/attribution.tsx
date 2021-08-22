6beacebdf28e2459c0048034a98a9a957d0d1df9
Nikita Stroganov <findoss@yandex.ru>
2021-08-22 23:26:52 +0300

    add sponsor

---
+++ src/containers/map/tileLayer/attribution.tsx
@@ -0,0 +1,8 @@
+export const ATTRIBUTION = `
+  <a  target="_blank" href="./public/contributors.txt">
+    contributors
+  </a>
+  <a href="https://discord.gg/FyutvHpS" target="_blank" rel="noopener noreferrer">
+    <div className="sponsor" id="sponsor"></div>
+  </a>
+`;
