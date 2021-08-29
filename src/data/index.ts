e57746986929f2f8a292b32bb59b7da3552a7923
Nikita Stroganov <findoss@yandex.ru>
2021-08-29 19:27:00 +0300

    add LD map

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,8 +2,9 @@ import LABELS_SB from './names/SB.json';
 import ZONES_SB from './zone/SB.json';
 import CACHE_SB from './cache/SB.json';
 
-// import LABELS_LD from './names/LD.json';
-// import CACHE_LD from './cache/LD.json';
+import LABELS_LD from './names/LD.json';
+import CACHE_LD from './cache/LD.json';
+import ZONES_LD from './zone/LD.json';
 
 // import LABELS_DS from './names/DS.json';
 // import CACHE_DS from './cache/DS.json';
@@ -36,19 +37,19 @@ type TypeFeatureMarkerCollection = Record<string, TypeCollectionFeatures>;
 
 const labels: TypeFeatureMarkerCollection = {
   SB: LABELS_SB as any,
-  // LD: LABELS_LD,
+  LD: LABELS_LD as any,
   // DS: LABELS_DS,
 };
 
 const cache: TypeFeatureMarkerCollection = {
   SB: CACHE_SB as any,
-  // LD: CACHE_LD,
+  LD: CACHE_LD as any,
   // DS: TMP,
 };
 
 const zones: TypeFeatureMarkerCollection = {
   SB: ZONES_SB as any,
-  // LD: LABELS_LD,
+  LD: ZONES_LD as any,
   // DS: LABELS_DS,
 };
 
--- src/store/map/state.ts
+++ src/store/map/state.ts
@@ -39,24 +39,24 @@ export const initialState: MapState = {
       center: [-500, 500],
       padding: 300,
     },
-    // LD: {
-    //   id: 'LD',
-    //   image: {
-    //     width: 4096,
-    //     height: 4096,
-    //     path: '/images/tiles/LD/{z}-{x}-{y}.jpg',
-    //   },
-    //   levels: {
-    //     org: 4,
-    //     max: 7,
-    //     min: 1,
-    //     default: 1,
-    //   },
-    //   width: 1000,
-    //   height: 1000,
-    //   center: [-500, 500],
-    //   padding: 300,
-    // },
+    LD: {
+      id: 'LD',
+      image: {
+        width: 4096,
+        height: 4096,
+        path: '/images/tiles/LD/{z}-{x}-{y}.jpg',
+      },
+      levels: {
+        org: 4,
+        max: 7,
+        min: 1,
+        default: 1,
+      },
+      width: 1000,
+      height: 1000,
+      center: [-500, 500],
+      padding: 300,
+    },
     // DS: {
     //   id: 'DS',
     //   image: {
