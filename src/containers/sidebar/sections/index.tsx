08b6f5a28c8163ee92e6d74fb43d32fc998cadd3
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-02-25 21:50:35 +0300

    стилизация sections

--- src/containers/sidebar/sections/index.tsx
+++ src/containers/sidebar/sections/index.tsx
@@ -1,4 +1,4 @@
-import classNames from 'classnames';
+import cn from 'classnames';
 
 import { SectionLang } from './lang';
 import { SectionMaps } from './maps';
@@ -9,28 +9,33 @@ import { SectionEditor } from './panel';
 import { INVITE_LINK_DISCORD_RUHUNT } from '../../../constants';
 
 import { ReactComponent as SponsorSVG } from '../../../assets/sponsor/ruhuntwhite.svg';
-import './style.css';
+import styles from './sections.module.css';
 
 type Props = {
   className?: string;
 };
 
 export const Sections = ({ className }: Props) => {
-  const styleSections = classNames('sections', className);
+  const styleSections = cn(`${styles.section}`, className);
+  const styleSectionLang = cn(`${styles.section} ${styles.section__lang}`);
+  const styleSection = cn(`${styles.section}`);
+  //что такое -br и __adv? Не понял смысла сокращения.
+  const styleSectionBr = cn(`${styles.section__br}`);
+  const styleSectionAdv = cn(`${styles.section__adv}`);
 
   return (
     <>
       <div className={styleSections}>
-        <SectionLang className="section section__lang" />
-        <SectionMaps className="section section__map-switch" />
-        <SectionFilters className="section" />
-        <SectionTools className="section" />
-        <SectionEditor className="section" />
-        <div className="section section-br" />
+        <SectionLang className={styleSectionLang} />
+        <SectionMaps className={styleSection} />
+        <SectionFilters className={styleSection} />
+        <SectionTools className={styleSection} />
+        <SectionEditor className={styleSection} />
+        <div className={styleSectionBr} />
       </div>
 
       <a
-        className="section section__adv"
+        className={styleSectionAdv}
         href={INVITE_LINK_DISCORD_RUHUNT}
         target="_blank"
         rel="noopener noreferrer"
