7945433431e5eee554fc1befc7ca43d872d5a0be
Nikita Stroganov <findoss@yandex.ru>
2022-03-14 22:54:34 +0300

    add ver app

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export const INVITE_LINK_DISCORD_RUHUNT = 'https://discord.gg/EfdknysD';
 export const IS_DEV = process.env.NODE_ENV === 'development';
 export const IS_PROD = process.env.NODE_ENV === 'production';
+export const VERSION_APP = process.env.REACT_APP_VERSION;
