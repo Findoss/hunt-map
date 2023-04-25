cffbdaba945d39e94d2a032f689f345a7e4fc83b
Nikita Stroganov <findoss1@gmail.com>
2023-04-25 21:48:48 +0300

    up deps

--- src/components/switch/index.ts
+++
@@ -1,8 +0,0 @@
-import { ReactElement } from 'react';
-
-export const Switch = (props: any) => {
-  const { test, children } = props;
-  return children.find((child: ReactElement) => {
-    return child.props.value === test;
-  });
-};
